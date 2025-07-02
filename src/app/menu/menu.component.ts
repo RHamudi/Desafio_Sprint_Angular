c23e217e3acf5a0cecbc3f62daa3f2dbd3a7ecd2
RHamudi <ramonramos.silva16@gmail.com>
2025-07-02 16:08:53 -0300

    feat: adicionando verificação de rota e lancando evento para background

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { Router, RouterLink } from '@angular/router';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-menu',
@@ -9,17 +9,25 @@ import { Router, RouterLink } from '@angular/router';
   styleUrl: './menu.component.css'
 })
 export class MenuComponent {
+   activeRoute: string;
 
-  constructor(private route: Router) {}
+  constructor(private route: Router, private activatedRoute: ActivatedRoute) {
+    this.activeRoute = this.activatedRoute.snapshot.url.join('/');
+    console.log(this.activeRoute);
+  }
+
+  @Output() modalStatusChange = new EventEmitter<boolean>();
 
   showModal = false;
 
   toggleModal() {
     this.showModal = true;
+    this.modalStatusChange.emit(this.showModal);
   }
 
   closeModal() {
     this.showModal = false;
+    this.modalStatusChange.emit(this.showModal);
   }
 
   logout() {
